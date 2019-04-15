b5d958467e853a7cae36ee297046cd490a88ef2e
gowhtmaMK2000 <mkbegins@outlook.com>
2019-04-16 02:50:05 +0530

    R commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn anonymouswishes.wsgi:application --preload --workers 1
+web: gunicorn anonymouswishes.wsgi --log-file -
--- anonymoussite/settings.py
+++ anonymoussite/settings.py
@@ -131,6 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_TMP = os.path.join(BASE_DIR, 'static')
 
 MIDDLEWARE_CLASSES = [
+'django.middleware.security.SecurityMiddleware',
 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -139,5 +140,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 django_heroku.settings(locals())
 
