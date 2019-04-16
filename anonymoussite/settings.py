f7698a5bcb14c2e738f0dc6be85d131cf9941cba
gowhtmaMK2000 <mkbegins@outlook.com>
2019-04-16 10:38:35 +0530

    settings update

--- anonymoussite/settings.py
+++ anonymoussite/settings.py
@@ -82,18 +82,21 @@ WSGI_APPLICATION = 'anonymoussite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-  #      'ENGINE': 'django.db.backends.sqlite3',
-   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
-#}
-
 DATABASES = {
-      'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+#DATABASES = {
+ #     'default': dj_database_url.config(
+  #        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+      #)
+#  }
 
  
 
