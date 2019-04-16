9787632b7aff698041967b07f233817653f35767
gowhtmaMK2000 <mkbegins@outlook.com>
2019-04-16 11:09:09 +0530

    static update

--- a_site/urls.py
+++ a_site/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 app_name='a_site'
 
 urlpatterns=[
@@ -9,4 +10,4 @@ urlpatterns=[
 	path('validate/',views.validate,name='validate'),
 	path('insert/',views.insert,name='insert'),
 	path('wishes/',views.wishes,name='wishes'),
-]
+] + static(settings.STATIC_URL,decument_root=settings.STATIC_ROOT)
