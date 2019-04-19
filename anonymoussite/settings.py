a4d67417a376bc588d1636a7d8a6daee8909d094
gowhtmaMK2000 <mkbegins@outlook.com>
2019-04-19 13:43:19 +0530

    db change

--- anonymoussite/settings.py
+++ anonymoussite/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'anonymoussite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'a_site',
+	'USER': 'mk',
+	'PASSWORD': 'mk',
+	'HOST': '',
+	'PORT': '5432'
     }
 }
 
